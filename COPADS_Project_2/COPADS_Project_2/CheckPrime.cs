29edf36a58a08dafa689ef2923228fae8d6b3214
Isaias Villalobos <isaiasvillalobos98@gmail.com>
2019-10-08 00:15:11 -0400

    Changes the format of the code and added the functionality of Parallel processing

---
+++ COPADS_Project_2/COPADS_Project_2/CheckPrime.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Numerics;
+
+namespace COPADS_Project_2
+{
+    public class Extension
+    {
+
+        public bool IsProbablyPrime(BigInteger value, int witnesses = 10)
+        {
+            if (value <= 1) return false;
+            if (witnesses <= 0) witnesses = 10;
+            BigInteger d = value - 1;
+            int s = 0;
+            while (d % 2 == 0)
+            {
+                d /= 2;
+                s += 1;
+            }
+            Byte[] bytes = new Byte[value.ToByteArray().LongLength];
+            BigInteger a;
+            for (int i = 0; i < witnesses; i++)
+            {
+                do
+                {
+                    var Gen = new Random();
+                    Gen.NextBytes(bytes);
+                    a = new BigInteger(bytes);
+                } while (a < 2 || a >= value - 2);
+                BigInteger x = BigInteger.ModPow(a, d, value);
+                if (x == 1 || x == value - 1) continue;
+                for (int r = 1; r < s; r++)
+                {
+                    x = BigInteger.ModPow(x, 2, value);
+                    if (x == 1) return false;
+                    if (x == value - 1) break;
+                }
+                if (x != value - 1) return false;
+            }
+            return true;
+        }
+
+    }
+}
--- COPADS_Project_2/COPADS_Project_2/Program.cs
+++ COPADS_Project_2/COPADS_Project_2/Program.cs
@@ -1,12 +1,126 @@
 ﻿using System;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
 
 namespace COPADS_Project_2
 {
-    class Program
+    public class Program
     {
+        static object locker = new object();
+        public static BigInteger GeneratePrimeNumber(int bitsArgument)
+        {
+            RNGCryptoServiceProvider randomNumbers = new RNGCryptoServiceProvider();
+
+            byte[] byteArray = new Byte[bitsArgument / 8];
+
+            //Setter function
+            randomNumbers.GetNonZeroBytes(byteArray);
+
+            //Set the constructor of the bigInteger, return bigInt
+            BigInteger bigInt = new BigInteger(byteArray);
+
+            return bigInt;
+
+        }
+        public static bool checkPrimeNumber(BigInteger bigInt)
+        {
+            var checkPrime = new Extension();
+            var isPrime = checkPrime.IsProbablyPrime(bigInt);
+            return isPrime;
+        }
+
+        static void serialPrimeNumber(int bitsArgument, int tracker, int countsArgument)
+        {
+            //loop until your find 'count' many prime numbers
+            while (tracker != countsArgument)
+            {
+                var primeNumber = GeneratePrimeNumber(bitsArgument);
+                var prime = checkPrimeNumber(primeNumber);
+
+                //If number is prime then your should print.
+                if (prime)
+                {
+                    tracker += 1;
+                    Console.WriteLine(primeNumber);
+                }
+            }
+        }
+        static void parallelPrimeFunction(int bitsArgument, int countsArgument)
+        {
+            var tracker = 0;
+            Parallel.For(0, countsArgument, i =>
+            {
+
+                while (tracker != countsArgument)
+                {
+                    var primeNumber = GeneratePrimeNumber(bitsArgument);
+                    var prime = checkPrimeNumber(primeNumber);
+
+                    //If number is prime then your should print.
+                    if (prime)
+                    {
+                        tracker += 1;
+                        Console.WriteLine(primeNumber);
+                    }
+                }
+
+            });
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //This will make sure to check for input less than 2 and more than 2
+            if (args.Length < 1 || args.Length > 2)
+            {
+                Console.Error.WriteLine("dotnet run PrimeGen <bits> <count = 1>");
+                Console.Error.WriteLine("- bits - the number of bits of the prime number, this must be a");
+                Console.Error.WriteLine("\tmultiple of 8, and at least 32 bits.");
+                Console.Error.WriteLine("- count - the number of prime numbers to generate, defaults to 1>");
+            }
+            else
+            {
+                //Process the argument with exactly 1 input argument
+                if (args.Length == 1)
+                {
+                    Console.WriteLine("Using default count argument (count == 1)");
+
+                    var bitsArgument = Convert.ToInt32(args[0].ToString());
+                    var countsArgument = 1;
+                    var tracker = 0;
+
+                    //The number is multiple of 8
+                    if (bitsArgument % 8 == 0 && bitsArgument >= 32)
+                    {
+                        serialPrimeNumber(bitsArgument, tracker, countsArgument);
+                    }
+                    else
+                    {
+                        Console.Error.WriteLine("The number is needs to be at least 32 bits and multiple of 8");
+                    }
+                }
+
+                //Make sure to process the two arguments
+                if (args.Length == 2)
+                {
+                    //check if first parameter is valid
+                    var bitsArgument = Convert.ToInt32(args[0].ToString());
+                    var countsArgument = Convert.ToInt32(args[1].ToString());
+
+
+                    //The number is multiple of 8
+                    if (bitsArgument % 8 == 0 && bitsArgument >= 32)
+                    {
+                        parallelPrimeFunction(bitsArgument, countsArgument);
+                    }
+                    else
+                    {
+                        Console.Error.WriteLine("The number is needs to be at least 32 bits and multiple of 8");
+                    }
+                }
+
+            }
         }
     }
 }
